5aa0324a39999c2c6fc1c8213155b34313688975
Shane C. Henderson <schender@ncsu.edu>
2019-08-01 23:22:38 -0400

    Add the first file

---
+++ project_C.py
@@ -0,0 +1,20 @@
+# COSC 505: Summer 2019
+# ProjectC - Group 14
+# Damilola Olayinka Akamo
+# Nicole Callais
+# Shane Christopher Henderson
+# Stephen Opeyemi Fatokun
+#
+# 08/01/2019
+import pandas as pd
+
+### Code for Data Analysis ###
+
+
+### Code for the http request ###
+
+
+### Code for input and output ###
+
+
+if __name__ = "__main__":
