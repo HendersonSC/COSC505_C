eb4583d3ac5d00f77eed497286f0b530cbf9cd0b
Shane C. Henderson <schender@ncsu.edu>
2019-08-04 15:10:49 -0400

    Add the initial bits of functionality

--- project_C.py
+++ project_C.py
@@ -6,15 +6,53 @@
 # Stephen Opeyemi Fatokun
 #
 # 08/01/2019
+import argparse
 import pandas as pd
+from urllib.error import URLError
 
 ### Code for Data Analysis ###
 
+### Code for for serving http request and getting data ###
+#shende25
+def get_dataset(url):
+    try:
+        df = pd.read_json(url)
+    except URLError as e:
+        # Catch errors related to the URL
+        if hasattr(e, 'reason'):
+            print('Connection failed!')
+            print('Reason: ', e.reason)
 
-### Code for the http request ###
+        # Catch errors related to the server
+        elif hasattr(e, 'code'):
+            print('Server Error!')
+            print('Error code: ', e.code)
+        return None
+    else:
+        return df
+
+### Code for output ###
 
 
-### Code for input and output ###
 
+### Code for parsing input ###
+#shende25
+def setup():
+    parser = argparse.ArgumentParser(description="Scans sample medical data \
+            for correlations, input data should be in .JSON format.", \
+            add_help=False)
+    parser.add_argument('data',metavar='<URL>', nargs=1, \
+            type=lambda x: get_dataset(x),\
+            help="The url where the data file can be found")
+    parser.add_argument('output',metavar='<output file or ->', nargs=1, \
+            type=argparse.FileType('w'), \
+            help="Location for writing output, '-' will direct to stdout")
+    args = parser.parse_args()
+    print(args.output)
+    print(args.data,file=args.output[0])
+    return args.data,args.output[0]
 
-if __name__ = "__main__":
+### Default usage
+#shend25
+if __name__ == "__main__":
+    df,outfile = setup()
